c1fdb0634057f135615e6636893628151e0a2b66
Wasif0787 <wasifhussain787@gmail.com>
2024-02-08 15:25:30 +0530

    Added Day 08

---
+++ Day 08/positiveIntegerHandler.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+function positiveIntegerHandler(req, res, next) {
+    const number = parseInt(req.query.number);
+    if (Number.isInteger(number) && number > 0) {
+        console.log(`Number ${number} is Positive`);
+        res.status(200).json({ message: `Success: Number ${number} is positive` })
+    } else {
+        const error = new Error(`Number ${number} must be a positive integer.`);
+        error.status = 400;
+        next(error);
+    }
+}
+function errorHandler(err, req, res, next) {
+    console.log(err.message);
+    res.status(err.status || 500).json({ error: err.message })
+}
+app.get("/", (req, res) => {
+    res.send("Hello");
+});
+app.get("/positive", positiveIntegerHandler);
+
+app.use(errorHandler);
+
+app.listen(PORT, () => {
+    console.log(`Listening to Port ${PORT}`);
+});
